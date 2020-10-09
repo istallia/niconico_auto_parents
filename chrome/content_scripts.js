4e4fc5d7d8f4edab00df44be201f042255ddeba3
Istallia <55042065+istallia@users.noreply.github.com>
2020-10-09 14:44:03 +0900

    [Add] ページにボタンを追加

--- chrome/content_scripts.js
+++ chrome/content_scripts.js
@@ -2,3 +2,12 @@
  * ページに作用するJavaScript
  * ボタンやモーダルウィンドウの追加、および動作の記述を行う
  */
+
+/* --- ページに要素を追加する --- */
+/* ボタンの追加 */
+let button_open = document.createElement('a');
+button_open.classList.add('btn-01');
+button_open.innerText    = '[拡張機能]IDリストから自動登録';
+button_open.style.cursor = 'pointer';
+let parent_button        = document.getElementsByClassName('submit')[0];
+parent_button.insertBefore(button_open, parent_button.firstChild);
