355c3099da13bdb29ca5a55dfed67964fcf4857c
Istallia <55042065+istallia@users.noreply.github.com>
2020-10-12 08:34:51 +0900

    [Add] IDリストを最高効率に変換する関数を完成

--- chrome/content_scripts.js
+++ chrome/content_scripts.js
@@ -7,7 +7,32 @@
 let ista_processing = false;
 
 /* --- IDリストを最高効率に変換する --- */
-let optimize_list = id_list => {};
+let optimize_list = id_list => {
+	/* IDを1つずつバラバラの配列にする */
+	let p_list = id_list.split('\n');
+	for (i in p_list) p_list[i] = p_list[i].split();
+	p_list.flat();
+	/* IDの形式であり、かつ重複のないリストを作成する */
+	let ok_list = [];
+	for (i in p_list) {
+		if( /^[a-zA-Z]{1,3}\d{1,12}$/.test(p_list[i]) && ok_list.indexOf(p_list[i]) < 0 ) {
+			ok_list.push(p_list[i]);
+		}
+	}
+	/* 10件ごとのリストに変換する */
+	let res_list = [];
+	for (i in ok_list) {
+		i = Number(i);
+		if( i % 10 === 0 ) res_list.push('');
+		res_list[Math.floor(i/10)] += ok_list[i];
+		if( i % 10 < 9 ) {
+			res_list[Math.floor(i/10)] += ' ';
+		} else {
+			res_list[Math.floor(i/10)] += '\n';
+		}
+	}
+	return res_list;
+};
 
 /* --- 入力→候補に追加→要素の移動 を行うPromiseを返す --- */
 let create_promise_candidates = id10 => {
