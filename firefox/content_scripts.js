af84f60fff3b21b22348c54c4767cdd4dd479ad9
Istallia <55042065+istallia@users.noreply.github.com>
2020-10-14 16:57:42 +0900

    [Add] Firefoxアドオン用ファイルも一応追加

---
+++ firefox/content_scripts.js
@@ -0,0 +1,155 @@
+/*
+ * ページに作用するJavaScript
+ * ボタンやモーダルウィンドウの追加、および動作の記述を行う
+ */
+
+/* --- 状態を保存する --- */
+let ista_processing = false;
+
+/* --- IDリストを最高効率に変換する --- */
+let optimize_list = id_list => {
+	/* IDを1つずつバラバラの配列にする */
+	let p_list = id_list.split('\n');
+	for (i in p_list) p_list[i] = p_list[i].split(' ');
+	p_list = p_list.flat();
+	/* 既に登録済みのリストを取得する */
+	let ng_list = [];
+	let items   = [... document.getElementById('parents').children];
+	for (item of items) ng_list.push(item.id);
+	/* IDの形式であり、かつ重複のないリストを作成する */
+	let ok_list = [];
+	for (i in p_list) {
+		if( /^[a-zA-Z]{1,3}\d{1,12}$/.test(p_list[i]) && ok_list.indexOf(p_list[i]) < 0 && ng_list.indexOf(p_list[i]) < 0 ) {
+			ok_list.push(p_list[i]);
+		}
+	}
+	/* 10件ごとのリストに変換する */
+	let res_list = [];
+	for (i in ok_list) {
+		i = Number(i);
+		if( i % 10 === 0 ) res_list.push('');
+		res_list[Math.floor(i/10)] += ok_list[i];
+		if( i % 10 < 9 ) {
+			res_list[Math.floor(i/10)] += ' ';
+		} else {
+			res_list[Math.floor(i/10)] += '\n';
+		}
+	}
+	for (i in res_list) {
+		res_list[i] = res_list[i].slice(0, -1);
+	}
+	return res_list;
+};
+
+/* --- 入力→候補に追加→要素の移動 を行うPromiseを返す --- */
+let create_promise_candidates = id10 => {
+	return new Promise(resolve => {
+		/* 入力 */
+		return new Promise(() => {
+			// console.log(id10);
+			document.getElementById('candidate_input').value = id10;
+			resolve();
+		});
+	})
+	.then(() => {
+		/* 候補に追加 */
+		return new Promise(resolve => {
+			let button = document.querySelector('a[title="候補に追加する"]');
+			button.dispatchEvent(new Event('click', {bubbles: true, composed: true}));
+			// console.log('[拡張機能]候補に追加');
+			setTimeout(() => {
+				document.getElementById('candidate_input').value = '';
+			}, 200);
+			resolve();
+		});
+	})
+	.then(() => {
+		/* 要素の移動 */
+		return new Promise(resolve => {
+			let add_candidates = count => {
+				let candidates   = document.getElementById('candidate');
+				let parent_works = document.getElementById('parents');
+				let items        = [... candidates.children];
+				console.log(items);
+				if( items.length < 1 && count < 10 ) {
+					setTimeout(add_candidates.bind(this, count+1), 200);
+					console.log('setTimeout');
+					return;
+				}
+				items.forEach(item => {
+					parents.appendChild(item);
+					console.log(item);
+				});
+				console.log('[拡張機能]要素の移動');
+				resolve();
+			};
+			setTimeout(add_candidates.bind(this, 0), 300);
+		});
+	});
+};
+
+/* --- IDリストを読み取り、すべてのIDを追加する --- */
+let add_materials = () => {
+	ista_processing  = true;
+	let id_list      = document.getElementById('ista-auto-list').value;
+	id_list          = optimize_list(id_list);
+	document.getElementById('ista-auto-list').value = id_list.join('\n');
+	// let promise_list = [];
+	let a_promise = Promise.resolve();
+	for (const list of id_list) {
+		// promise_list.push(create_promise_candidates.bind(this, list));
+		if( list.length > 2 ) a_promise = a_promise.then(create_promise_candidates.bind(this, list));
+	}
+	// let a_promise = promise_list.reduce((promise, task) => (
+	// 	promise.then(task)
+	// ), Promise.resolve());
+	a_promise.then(() => {
+		document.getElementById('ista-auto-modal').style.display    = 'none';
+		document.getElementById('ista-auto-modal-bg').style.display = 'none';
+		document.getElementById('ista-auto-list').value             = '';
+		document.getElementById('checkbox').style.display           = 'none';
+		document.getElementById('parents').style.backgroundImage    = 'url("")';
+		ista_processing                                             = false;
+	});
+};
+
+/* --- ページに要素を追加する --- */
+/* ボタンの追加 */
+let button_open = document.createElement('a');
+button_open.classList.add('btn-01');
+button_open.innerText    = '[拡張機能]IDリストから自動登録';
+button_open.style.cursor = 'pointer';
+button_open.addEventListener('click', () => {
+	document.getElementById('ista-auto-modal').style.display    = 'block';
+	document.getElementById('ista-auto-modal-bg').style.display = 'block';
+	document.getElementById('ista-auto-button').onclick         = add_materials;
+	let select           = document.getElementById('site_selector');
+	select.selectedIndex = select.options.length - 1;
+	select.dispatchEvent(new Event('change', {bubbles: true, composed: true}));
+});
+let parent_button = document.getElementsByClassName('submit')[0];
+parent_button.insertBefore(button_open, parent_button.firstChild);
+/* モーダルウィンドウの追加 */
+let modal_win = document.createElement('div');
+modal_win.id = 'ista-auto-modal';
+modal_win.classList.add('ista-auto-modal');
+modal_win.innerHTML = `
+<p>
+	使用した素材のIDのリストを入力してください。<br>
+	[v0.1.1] IDのリストの整理は自動で行います。1行が10件未満、または超過でも10件ごとに整頓します。
+</p>
+<textarea id="ista-auto-list" rows="8"></textarea>
+<button id="ista-auto-button">自動登録</button>
+`;
+parent_button.parentNode.insertBefore(modal_win, parent_button);
+/* モーダル背景の追加 */
+let modal_win_bg = document.createElement('div');
+modal_win_bg.id  = 'ista-auto-modal-bg';
+modal_win_bg.classList.add('ista-auto-modal-bg');
+parent_button.parentNode.insertBefore(modal_win_bg, parent_button);
+modal_win_bg.addEventListener('click', () => {
+	if( !ista_processing ) {
+		document.getElementById('ista-auto-modal').style.display    = 'none';
+		document.getElementById('ista-auto-modal-bg').style.display = 'none';
+	}
+});
