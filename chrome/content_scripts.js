3de303e2c7bb8cca3f26fd90d55e87ca4cfb9f74
Istallia <55042065+istallia@users.noreply.github.com>
2021-03-22 20:43:49 +0900

    [Modify] ニコニコ・ブックマークのボタンのスタイルを地味に

--- chrome/content_scripts.js
+++ chrome/content_scripts.js
@@ -241,10 +241,10 @@ modal_win.innerHTML = `
 <p>
 	使用した素材のIDのリストを入力してください。1行が10件未満、または11件以上になっている場合は自動で整頓されます。<br>
 	ファイル(複数可)を枠内にD&Dすると、そのファイルの中身または名前に含まれる作品ID(動画/静画/コモンズ/立体)を抽出できます。<br>
-	<button type="button" id="ista-open-sidebar-bookmarks" title="(v0.4.0) ブラウザのブックマークから作品IDを選択して追加します。">ニコニコ・ブックマーク</button> <label for="ista-verify-contents" title="(v0.3.2) これがOnのとき、親作品に登録できなかった作品を自動で確認してお知らせします。"><input type="checkbox" id="ista-verify-contents" checked>&nbsp;書き込み検証を行う</label>
+	<button type="button" id="ista-open-sidebar-bookmarks" class="ista-button white" title="(v0.4.0) ブラウザのブックマークから作品IDを選択して追加します。">ニコニコ・ブックマーク</button> <label for="ista-verify-contents" title="(v0.3.2) これがOnのとき、親作品に登録できなかった作品を自動で確認してお知らせします。"><input type="checkbox" id="ista-verify-contents" checked>&nbsp;書き込み検証を行う</label>
 </p>
 <textarea id="ista-auto-list"></textarea>
-<button id="ista-auto-button">自動登録</button>
+<button id="ista-auto-button" class="ista-button">自動登録</button>
 `;
 parent_button.parentNode.insertBefore(modal_win, parent_button);
 /* モーダル背景の追加 */
@@ -256,6 +256,7 @@ modal_win_bg.addEventListener('click', () => {
 	if( !ista_processing ) {
 		document.getElementById('ista-auto-modal').style.display    = 'none';
 		document.getElementById('ista-auto-modal-bg').style.display = 'none';
+		document.getElementById('ista-sidebar-bookmarks').classList.remove('visible');
 	}
 });
 /* 「一括登録」ボタンの追加 */
