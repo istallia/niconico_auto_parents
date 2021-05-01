56cbcb849383e521dc2c9248f16fddd96de23ba3
Istallia <55042065+istallia@users.noreply.github.com>
2021-05-01 17:00:56 +0900

    [Add] ファイルからのID抽出を予約でもできるように

--- chrome/tree-view.js
+++ chrome/tree-view.js
@@ -29,6 +29,32 @@ const reserveParents = () => {
 };
 
 
+/* --- [予約投稿] IDリストをファイルから抽出する --- */
+const extractIDsFromFiles = event => {
+	/* ファイルからID抽出 */
+	if (event.dataTransfer.files.length <= 0) return;
+	event.preventDefault();
+	document.getElementById('ista-form-reserving-ids').classList.remove('ista-hover');
+	const extract_func = (name, event) => {
+		const regexp       = /(?<=^|[^a-zA-Z0-9])((nc|im|sm|td)\d{2,12})(?=[^a-zA-Z0-9]|$)/g;
+		const dropped_text = event.currentTarget.result.replace(/\x00/g, '');
+		let ids_in_name  = [... name.matchAll(regexp)];
+		let dropped_ids  = [... dropped_text.matchAll(regexp)];
+		if (ids_in_name.length > 0) dropped_ids = ids_in_name;
+		for (let index in dropped_ids) dropped_ids[index] = dropped_ids[index][1];
+		let text_list = document.getElementById('ista-form-reserving-ids').value;
+		if (text_list.slice(-1) !== '\n' && text_list.length > 0) text_list = text_list + '\n';
+		text_list = text_list + optimizeList(dropped_ids.join(' '),false).join('\n');
+		document.getElementById('ista-form-reserving-ids').value = text_list;
+	}
+	for (let file of event.dataTransfer.files) {
+		const reader = new FileReader();
+		reader.addEventListener('load', extract_func.bind(this, file.name));
+		reader.readAsText(file);
+	}
+};
+
+
 /* --- [予約投稿] エラーページに予約投稿用のフォームを追加 --- */
 const addReservingParentsForm = () => {
 	/* URLを確認 */
@@ -55,9 +81,12 @@ const addReservingParentsForm = () => {
 	form_desc.innerText      = '設定した時刻に自動でタブを開き、IDリストを流し込みます。\n※指定時刻にPCを起動しておく必要があります。';
 	input_date.type          = 'date';
 	input_time.type          = 'time';
-	input_ids.placeholder    = 'ここにIDリストを入力';
+	input_ids.placeholder    = 'ここにIDリストを入力\nファイルをD&DするとIDを抽出します(複数ファイル可)';
 	button_reserve.innerText = '[拡張機能] ツリー登録予約';
 	button_reserve.href      = 'javascript:void(0)';
+	input_ids.addEventListener('dragover' , event => event.currentTarget.classList.add('ista-hover')   );
+	input_ids.addEventListener('dragleave', event => event.currentTarget.classList.remove('ista-hover'));
+	input_ids.addEventListener('drop'     , extractIDsFromFiles);
 	button_reserve.addEventListener('click', reserveParents);
 	li.appendChild(form_title);
 	li.appendChild(form_desc);
@@ -129,4 +158,36 @@ const analyzeGetParam = query => {
 	params = params.map(param => param.split('='));
 	params.forEach(param => result[param[0]] = decodeURI(param[1]));
 	return result;
+};
+
+
+/* --- IDリストを最高効率に変換する --- */
+let optimizeList = (id_list, check_parents = true) => {
+	/* IDを1つずつバラバラの配列にする */
+	let p_list = id_list.split('\n');
+	for (i in p_list) p_list[i] = p_list[i].split(' ');
+	p_list = p_list.flat();
+	/* IDの形式であり、かつ重複のないリストを作成する */
+	let ok_list = [];
+	for (i in p_list) {
+		if( /^[a-zA-Z]{1,3}\d{1,12}$/.test(p_list[i]) && ok_list.indexOf(p_list[i]) < 0 ) {
+			ok_list.push(p_list[i]);
+		}
+	}
+	/* 10件ごとのリストに変換する */
+	let res_list = [];
+	for (i in ok_list) {
+		i = Number(i);
+		if( i % 10 === 0 ) res_list.push('');
+		res_list[Math.floor(i/10)] += ok_list[i];
+		if( i % 10 < 9 ) {
+			res_list[Math.floor(i/10)] += ' ';
+		} else {
+			res_list[Math.floor(i/10)] += '\n';
+		}
+	}
+	for (i in res_list) {
+		res_list[i] = res_list[i].slice(0, -1);
+	}
+	return res_list;
 };
