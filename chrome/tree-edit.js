858910fe1347aff1b49dbbb785d1b4dd97d77d27
Istallia <55042065+istallia@users.noreply.github.com>
2021-07-09 23:30:44 +0900

    [Change] ニコニコ・ブックマークとツリー・チェインのボタン位置を入れ替え

--- chrome/tree-edit.js
+++ chrome/tree-edit.js
@@ -252,8 +252,8 @@ modal_win.innerHTML = `
 	ファイル(複数可)を枠内にD&Dすると、そのファイルの中身または名前に含まれる作品ID(動画/静画/コモンズ/立体)を抽出できます。
 </p>
 <button type="button" id="ista-read-parent-works" class="ista-button white" title="(v0.4.1) 親作品の欄にある作品を読み出します。">親作品を読み出し</button>&nbsp;
-<button type="button" id="ista-get-parents-of-parents" class="ista-button white" title="(v0.5.3) 選択範囲の作品の親作品を取得します。">ツリー・チェイン</button>&nbsp;
-<button type="button" id="ista-open-sidebar-bookmarks" class="ista-button white" title="(v0.4.2) ブラウザのブックマークから作品IDを選択して追加します。">ニコニコ・ブックマーク</button><br>
+<button type="button" id="ista-open-sidebar-bookmarks" class="ista-button white" title="(v0.4.2) ブラウザのブックマークから作品IDを選択して追加します。">ニコニコ・ブックマーク</button>&nbsp;
+<button type="button" id="ista-get-parents-of-parents" class="ista-button white" title="(v0.5.3) 選択範囲の作品の親作品を取得します。">ツリー・チェイン</button><br>
 <label for="ista-verify-contents" title="(v0.3.2) これがOnのとき、親作品に登録できなかった作品を自動で確認してお知らせします。"><input type="checkbox" id="ista-verify-contents" checked>&nbsp;書き込み検証を行う</label>
 <textarea id="ista-auto-list"></textarea>
 <button id="ista-auto-button" class="ista-button">自動登録</button>
