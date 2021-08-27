bb121c1b63d6f597009cf4aa198ff61d0b7c456e
Istallia <55042065+istallia@users.noreply.github.com>
2021-08-27 22:11:06 +0900

    [Replace] 関数名をキャメルケースに統一

--- chrome/tree-edit.js
+++ chrome/tree-edit.js
@@ -47,7 +47,7 @@ button_open.addEventListener('click', () => {
 			for (let index in dropped_ids) dropped_ids[index] = dropped_ids[index][1];
 			let text_list = document.getElementById('ista-textarea-id-list').value;
 			if (text_list.slice(-1) !== '\n' && text_list.length > 0) text_list = text_list + '\n';
-			text_list = text_list + optimize_list(dropped_ids.join(' '),false).join('\n');
+			text_list = text_list + optimizeList(dropped_ids.join(' '),false).join('\n');
 			document.getElementById('ista-textarea-id-list').value = text_list;
 		}
 		for (let file of event.dataTransfer.files) {
@@ -56,7 +56,7 @@ button_open.addEventListener('click', () => {
 			reader.readAsText(file);
 		}
 	};
-	document.getElementById('ista-button-auto-regist').onclick = add_materials;
+	document.getElementById('ista-button-auto-regist').onclick = addMaterialsByIdList;
 	let select           = document.getElementById('site_selector');
 	select.selectedIndex = select.options.length - 1;
 	select.dispatchEvent(new Event('change', {bubbles: true, composed: true}));
@@ -111,7 +111,7 @@ p_submit.insertBefore(notice_message, p_submit.firstChild);
 
 
 /* --- IDリストを最高効率に変換する --- */
-const optimize_list = (id_list, check_parents = true) => {
+const optimizeList = (id_list, check_parents = true) => {
 	/* IDを1つずつバラバラの配列にする */
 	let p_list = id_list.split('\n');
 	for (i in p_list) p_list[i] = p_list[i].split(' ');
@@ -148,7 +148,7 @@ const optimize_list = (id_list, check_parents = true) => {
 
 
 /* --- 入力→候補に追加→要素の移動 を行うPromiseを返す --- */
-const create_promise_candidates = id10 => {
+const createPromiseCandidates = id10 => {
 	return new Promise(resolve => {
 		/* 入力 */
 		return new Promise(() => {
@@ -199,7 +199,7 @@ const create_promise_candidates = id10 => {
 				/* 親作品欄との重複がないか確認 */
 				const p_items_id = p_items.map(li => li.id);
 				items            = items.filter(li => {
-					check_linked_commons(li);
+					checkLinkedCommons(li);
 					return p_items_id.indexOf(li.id) === -1;
 				});
 				/* 要素を親作品欄に追加 */
@@ -216,10 +216,10 @@ const create_promise_candidates = id10 => {
 
 
 /* --- IDリストを読み取り、すべてのIDを追加する --- */
-const add_materials = () => {
+const addMaterialsByIdList = () => {
 	ista_processing  = true;
 	let id_list      = document.getElementById('ista-textarea-id-list').value;
-	id_list          = optimize_list(id_list);
+	id_list          = optimizeList(id_list);
 	document.getElementById('ista-textarea-id-list').value = id_list.join('\n');
 	localStorage.setItem('ista-verify-contents', String(document.getElementById('ista-verify-contents').checked));
 	if (id_list.length > 0) {
@@ -229,8 +229,8 @@ const add_materials = () => {
 	// let promise_list = [];
 	let a_promise = Promise.resolve();
 	for (const list of id_list) {
-		// promise_list.push(create_promise_candidates.bind(this, list));
-		if( list.length > 2 ) a_promise = a_promise.then(create_promise_candidates.bind(this, list));
+		// promise_list.push(createPromiseCandidates.bind(this, list));
+		if( list.length > 2 ) a_promise = a_promise.then(createPromiseCandidates.bind(this, list));
 	}
 	// let a_promise = promise_list.reduce((promise, task) => (
 	// 	promise.then(task)
@@ -241,7 +241,7 @@ const add_materials = () => {
 			let dest_id = ista_linked_ids[source_id];
 			text_list = text_list.replace(source_id, dest_id);
 		}
-		let remained_list = optimize_list(text_list).join('\n');
+		let remained_list = optimizeList(text_list).join('\n');
 		let verify        = (localStorage.getItem('ista-verify-contents') !== 'false');
 		if ( remained_list.length > 2 && verify ) {
 			/* IDが残留した場合 */
@@ -261,7 +261,7 @@ const add_materials = () => {
 
 
 /* --- 現在の候補から一括登録を行う --- */
-const auto_reg_candidates = () => {
+const autoRegistCandidates = () => {
 	/* 各種要素を取得 */
 	let candidates   = document.getElementById('candidate');
 	let parent_works = document.getElementById('parents');
@@ -274,7 +274,7 @@ const auto_reg_candidates = () => {
 	}
 	/* 重複チェック＆追加 */
 	items.forEach(item => {
-		check_linked_commons(item);
+		checkLinkedCommons(item);
 		for(p_item of p_items) {
 			if(p_item.id === item.id) return;
 		}
@@ -285,12 +285,12 @@ const auto_reg_candidates = () => {
 		document.getElementById('parents').style.backgroundImage    = 'url("")';
 	}
 };
-button_regist_candidates.addEventListener('click', auto_reg_candidates);
-document.querySelector("#Column01 > div.tree-edit-area.editbox.round5-t > p.helpimg").addEventListener('click', auto_reg_candidates);
+button_regist_candidates.addEventListener('click', autoRegistCandidates);
+document.querySelector("#Column01 > div.tree-edit-area.editbox.round5-t > p.helpimg").addEventListener('click', autoRegistCandidates);
 
 
 /* --- 候補作品をクリックで移動させるためのイベント --- */
-const click_to_reg = event => {
+const clickToMoveCandidate = event => {
 	/* 親をチェック */
 	const parent = event.currentTarget.parentNode;
 	if( parent.id !== 'candidate' ) return;
@@ -302,7 +302,7 @@ const click_to_reg = event => {
 	}
 	/* 親作品欄との重複がないか確認 */
 	const p_items_id = p_items.map(li => li.id);
-	check_linked_commons(event.currentTarget);
+	checkLinkedCommons(event.currentTarget);
 	if (p_items_id.indexOf(event.currentTarget.id) > -1) return;
 	/* 移動する */
 	const parent_works = document.getElementById('parents');
@@ -316,7 +316,7 @@ let observer_candidates = () => {
 	let parent_items = [... document.getElementById('candidate').children];
 	parent_items.forEach(item => {
 		if( item.getAttribute('ev') !== 'yes' ) {
-			item.addEventListener('click', click_to_reg);
+			item.addEventListener('click', clickToMoveCandidate);
 			item.setAttribute('ev', 'yes');
 		}
 	});
@@ -340,7 +340,7 @@ button_open.addEventListener('click', () => {
 			const s_end    = textarea.selectionEnd;
 			id_text        = textarea.value.substring(s_start, s_end);
 		}
-		let id_list = optimize_list(id_text, false).join(' ').split(' ');
+		let id_list = optimizeList(id_text, false).join(' ').split(' ');
 		if (id_list.length < 1 || id_list[0].length < 3) return;
 		/* すべてのIDに対して親作品取得を実行 */
 		id_list.forEach(getParentsOfParents);
@@ -549,7 +549,7 @@ const readParentWorks = () => {
 	}
 	materials_list = materials_list.join('\n');
 	if (!window.confirm('タイトル付きの形式で読み出しますか？')) {
-		materials_list = optimize_list(materials_list, false).join('\n');
+		materials_list = optimizeList(materials_list, false).join('\n');
 	}
 	document.getElementById('ista-textarea-id-list').value = materials_list;
 };
@@ -559,7 +559,7 @@ button_open.addEventListener('click', () => {
 
 
 /* --- 連携付きコモンズ作品を連携先の表示に変更 --- */
-const check_linked_commons = (element, do_replace = true) => {
+const checkLinkedCommons = (element, do_replace = true) => {
 	/* 連携が付いているかチェック */
 	const main_creation   = element.querySelector('div.main-creation');
 	const linked_creation = element.querySelector('div.linked-creation');
