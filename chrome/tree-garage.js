b4ed6b1b695d43118f94fc64c0093e215c87dd54
Istallia <55042065+istallia@users.noreply.github.com>
2022-07-03 00:16:12 +0900

    [Modify] 読み込み中画像の表示方法を変更(できてない)

--- chrome/tree-garage.js
+++ chrome/tree-garage.js
@@ -37,15 +37,16 @@ const tree_ui_modal = parser.parseFromString(`
 		<div class="ista-parents-list">
 			<div class="ista-parent-work template" id="ncXXXXXXXX">
 				<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 1.5c-4.69 0-8.497 3.807-8.497 8.497s3.807 8.498 8.497 8.498 8.498-3.808 8.498-8.498-3.808-8.497-8.498-8.497zm0 7.425 2.717-2.718c.146-.146.339-.219.531-.219.404 0 .75.325.75.75 0 .193-.073.384-.219.531l-2.717 2.717 2.727 2.728c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.384-.073-.53-.219l-2.729-2.728-2.728 2.728c-.146.146-.338.219-.53.219-.401 0-.751-.323-.751-.75 0-.192.073-.384.22-.531l2.728-2.728-2.722-2.722c-.146-.147-.219-.338-.219-.531 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" fill-rule="nonzero"/></svg>
-				<img src="./benricho-359-66.gif" alt="サムネイル" class="ista-parent-img">
+				<img alt="サムネイル" class="ista-parent-img">
 				<a href="https://commons.nicovideo.jp" class="ista-parent-link" target="_blank"><span class="ista-parent-title">読み込み中...</span></a>&nbsp;
 				<span class="ista-parent-type">(読み込み中...)</span>
 			</div>
 		</div>
 	</div>
 `, 'text/html').querySelector('div.ista-tree-ui-modal');
-const tree_ui_loading_img = tree_ui_modal.querySelector('div.ista-parent-work.template > img');
-tree_ui_loading_img.src   = browser.runtime.getURL(tree_ui_loading_img.getAttribute('src'));
+const tree_ui_loading_img                  = tree_ui_modal.querySelector('div.ista-parent-work.template > img');
+tree_ui_loading_img.style.backgroundImage  = `url('${browser.runtime.getURL('benricho-359-66.gif')}')`;
+tree_ui_loading_img.style.backgroundRepeat = 'no-repeat';
 
 
 /* --- 広域変数 --- */
